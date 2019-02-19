39939d9a7f1354de330c906207842bf70e9d9b29
Mario Valenzuela <mariovalpa@gmail.com>
2019-02-19 00:57:38 -0700

    Se refactoriza código y se implementa funcion para clasificar digitos extraidos del test set y se muestra digito junto con el error de reconstruccion de cada autoencoder

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from keras.models import Model
 TRAINED_MODELS_PATH = './trained_models/'
 
 
-def plot_training_loss(training_history):
+def plot_training_loss(training_history, digit):
     plt.plot(training_history['loss'], linewidth=2, label='Train')
     plt.plot(training_history['val_loss'], linewidth=2, label='Test')
     plt.legend(loc='upper right')
-    plt.title('Model loss')
+    plt.title('Model loss for %s' % digit)
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.show()
@@ -42,7 +42,35 @@ def plot_output(data_test, decoded, n_samples):
     plt.show()
 
 
-def train_autoencoder_for_number(num_test):
+def get_train_test_data_for_digit(digit):
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+    digit_train_filter = np.where(y_train == digit)
+    others_train_filter = np.where(y_train != digit)
+
+    digit_x_train = x_train[digit_train_filter]
+    others_x_train = x_train[others_train_filter]
+
+    digit_test_filter = np.where(y_test == digit)
+    digit_x_test = x_test[digit_test_filter]
+
+    return digit_x_train, digit_x_test, others_x_train
+
+
+def get_random_test_images(n):
+    (_, _), (x_test, _) = mnist.load_data()
+    x_test = x_test.astype('float32') / 255.
+    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+    return x_test[np.random.choice(x_test.shape[0], n, replace=False), :]
+
+
+def train_autoencoder_for_number(digit):
     if not os.path.exists(TRAINED_MODELS_PATH):
         os.makedirs(TRAINED_MODELS_PATH)
 
@@ -62,7 +90,7 @@ def train_autoencoder_for_number(num_test):
 
     autoencoder.compile(optimizer='adadelta', loss='categorical_crossentropy')
 
-    cp = ModelCheckpoint(filepath=TRAINED_MODELS_PATH + 'auto_' + str(num_test) + '.h5',
+    cp = ModelCheckpoint(filepath=TRAINED_MODELS_PATH + 'auto_' + str(digit) + '.h5',
                          save_best_only=True,
                          verbose=0)
 
@@ -71,54 +99,40 @@ def train_autoencoder_for_number(num_test):
                      write_graph=True,
                      write_images=True)
 
-    ea = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+    ea = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 
-    # Procesamiento de datos
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    x_train = x_train.astype('float32') / 255.
-    x_test = x_test.astype('float32') / 255.
-    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-
-    # num_x_test = x_test
-    num_filter = np.where(y_train == num_test)
-    others_filter = np.where(y_train != num_test)
-    num_x_train = x_train[num_filter]
-    others_x_train = x_train[others_filter]
-    split_size = int(num_x_train.shape[0] * 0.7)
-    num_x_train, num_x_test = np.split(num_x_train, [split_size])
+    digit_x_train, digit_x_test, others_x_train = get_train_test_data_for_digit(digit)
 
     files = os.listdir(TRAINED_MODELS_PATH)
     exists = False
     for file in files:
-        if ('auto_' + str(num_test) + '.h5') == file:
+        if ('auto_' + str(digit) + '.h5') == file:
             exists = True
 
     if not exists:
-        history = autoencoder.fit(num_x_train, num_x_train,
+        history = autoencoder.fit(digit_x_train, digit_x_train,
                                   epochs=300,
-                                  batch_size=256,
+                                  batch_size=64,
                                   shuffle=True,
-                                  validation_data=(num_x_test, num_x_test),
+                                  validation_data=(digit_x_test, digit_x_test),
                                   callbacks=[cp, tb, ea]).history
-        plot_training_loss(history)
+        plot_training_loss(history, digit)
 
-    autoencoder = load_model(TRAINED_MODELS_PATH + 'auto_' + str(num_test) + '.h5')
-    num_x_reconstruction = autoencoder.predict(num_x_test)
+    autoencoder = load_model(TRAINED_MODELS_PATH + 'auto_' + str(digit) + '.h5')
+    num_x_reconstruction = autoencoder.predict(digit_x_test)
     others_x_reconstruction = autoencoder.predict(others_x_train)
 
-    num_mse = np.mean(np.power(num_x_test - num_x_reconstruction, 2), axis=1)
+    num_mse = np.mean(np.power(digit_x_test - num_x_reconstruction, 2), axis=1)
     others_mse = np.mean(np.power(others_x_train - others_x_reconstruction, 2), axis=1)
 
-    plot_output(num_x_test, num_x_reconstruction, 10)
+    plot_output(digit_x_test, num_x_reconstruction, 10)
 
     print("Creating error plot")
 
     threshold = np.mean(num_mse) + np.std(num_mse)
     fig, ax = plt.subplots()
 
-    ax.plot(num_mse, marker='o', ms=1.5, linestyle='', label="Digit %s" % num_test)
+    ax.plot(num_mse, marker='o', ms=1.5, linestyle='', label="Digit %s" % digit)
 
     others_mse = np.random.choice(others_mse, num_mse.size)
 
@@ -126,19 +140,19 @@ def train_autoencoder_for_number(num_test):
 
     ax.hlines(threshold, ax.get_xlim()[0], ax.get_xlim()[1], colors="r", zorder=100, label='Threshold')
     ax.legend()
-    plt.title("Reconstruction error for %s and others" % num_test)
+    plt.title("Reconstruction error for %s and others" % digit)
     plt.ylabel("Reconstruction error")
     plt.xlabel("Data point index")
     plt.show()
 
-    print("Hacer prediccion con todos los autoencoders")
-    image = num_x_test[0:1:1]
-    predict(image)
+    """print("Hacer prediccion con todos los autoencoders")
+    image = digit_x_test[0:1:1]
+    predict(image)"""
 
     # Evaluar el modelo
-    # mse = np.mean(np.power(num_x_test - num_x_reconstruction, 2), axis=1)
+    # mse = np.mean(np.power(digit_x_test - num_x_reconstruction, 2), axis=1)
     # error_df = pd.DataFrame({'Reconstruction_error': mse,
-    # 'True_class': num_x_test})
+    # 'True_class': digit_x_test})
     # error_df.describe()
 
 
@@ -162,4 +176,56 @@ def predict(image):
     print(reconstruction_errors)
 
 
-train_autoencoder_for_number(0)
+def classification_test():
+    test_images = get_random_test_images(10)
+
+    autoencoders = []
+    for i in range(10):
+        autoencoders.append(load_model(TRAINED_MODELS_PATH + 'auto_' + str(i) + '.h5'))
+
+    reconstruction_errors = []
+    for i in range(10):
+        reconstructions = autoencoders[i].predict(test_images)
+        mse = np.mean(np.power(test_images - reconstructions, 2), axis=1)
+        # plot_output(image, reconstruction, 1)
+        reconstruction_errors.append(mse)
+
+    reconstruction_errors = np.array(reconstruction_errors)
+
+    for i, image in enumerate(test_images):
+        plt.figure(figsize=(5, 4))
+        ax = plt.subplot(2, 1, 1)
+        plt.imshow(image.reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        row_labels = np.arange(10)
+        image_errors = reconstruction_errors[:, i]
+
+        min_index = np.argmin(image_errors)
+
+        cell_colors = []
+        for j in range(10):
+            cell_colors.append("w")
+
+        cell_colors[min_index] = "#66CC00"
+        cell_colors = np.array(cell_colors)
+
+        image_errors = image_errors.astype(np.str)
+
+        print(cell_colors)
+
+        plt.table(cellText=image_errors.reshape(10, 1), cellColours=cell_colors.reshape(10, 1), rowLabels=row_labels)
+
+        plt.title("Reconstruction error for each digit autoencoder")
+
+        plt.show()
+
+    print(reconstruction_errors)
+    print("==========")
+    print(reconstruction_errors[:, 1])
+
+
+classification_test()
+# train_autoencoder_for_number(0)
